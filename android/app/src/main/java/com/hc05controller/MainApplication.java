cc72c772daa56e5519f19b694a899f9181153a20
punisher-303 <mehafin0@gmail.com>
2025-06-15 18:20:25 +0530

    fixxx

--- android/app/src/main/java/com/hc05controller/MainApplication.java
+++ android/app/src/main/java/com/hc05controller/MainApplication.java
@@ -1,12 +1,17 @@
 package com.hc05controller;
 
 import android.app.Application;
-import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
 import java.util.List;
+
+// Import your required React Packages manually here
+import com.reactlibrary.BluetoothClassicPackage; // for react-native-bluetooth-classic
+// Add more packages as needed
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -19,7 +24,10 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-          return new PackageList(this).getPackages();
+          return Arrays.<ReactPackage>asList(
+            new BluetoothClassicPackage() // <-- Add your custom/native packages here
+            // new SomeOtherPackage() // Add more manually if needed
+          );
         }
 
         @Override
